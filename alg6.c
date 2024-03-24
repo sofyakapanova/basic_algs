73e399600e649c5c62886897843ed416757ed30f
sofyakapanova <105154076+sofyakapanova@users.noreply.github.com>
2024-03-25 00:52:27 +0300

    Create alg6.c

---
+++ alg6.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h> 
+int main() { 
+ char **name, **list; 
+ int n, k, i, l, j, t; 
+ char tmp[198]; 
+ printf("Press n: "); scanf("%d", &n); 
+ name = (char**)malloc(n * sizeof(char*)); list = (char**)malloc(n * sizeof(char*)); 
+ printf("Press data\n\n"); 
+ for (i = 0; i < n; i++) { 
+  printf("Press name: "); 
+  scanf("%s", tmp); 
+  name[i] = (char*)malloc((strlen(tmp) + 2)*sizeof(char)); 
+  strcpy(name[i], tmp);
+ } 
+ k = 0; 
+ for (i = 0; i < n; i++) { 
+  l = -1; 
+  j = 0; 
+  while ((l == -1) && (j < k)) { 
+   t = strcmp(list[j], name[i]); 
+   if (t == 0) { 
+    l = j; 
+   } 
+   j++; 
+  } 
+  if (l == -1) { 
+   list[k] = (char*)malloc((strlen(name[i]) + 2) * sizeof(char)); 
+   strcpy(list[k], name[i]);
+   k++;
+  }
+ } 
+ printf("\nList without repeat and her data: \n"); 
+ for (i = 0; i < k; i++) { 
+  printf("%s \n", list[i]); 
+ } 
+}
