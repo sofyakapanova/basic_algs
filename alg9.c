674c6f10075668d008f8e019767d7b383f5b4764
sofyakapanova <105154076+sofyakapanova@users.noreply.github.com>
2024-03-25 02:13:24 +0300

    Update alg9.c

--- alg9.c
+++ alg9.c
@@ -2,33 +2,19 @@
 #include <stdio.h>  
 #include <string.h>  
 #include <stdlib.h>  
- 
 int main() { 
-    char st[255]; 
-    char t[255]; 
-    char** w; 
-    char pr[2] = " \0"; 
-    int k, pos, i; 
- 
-    printf("Enter string: "); 
-    fgets(st, 255, stdin); 
-    w = (char**)malloc((strlen(st) + 200) * sizeof(char*)); 
+    char st[255]; char t[255]; char** W; char pr[2] = " \0"; int k, pos, i; 
+    printf("Enter string: "); fgets(st, 255, stdin); 
+    W = (char**)malloc((strlen(st) + 200) * sizeof(char*)); 
     k = 0; 
-    strcpy(t, st); 
-    strcat(t, pr); 
- 
+    strcpy(t, st); strcat(t, pr); // strcat склеивает
     while (strlen(t) > 0) { 
-        pos = strchr(t, ' ') - t; 
- 
+        pos = strchr(t, ' ') - t; // strchr - поиск 1 вхож-я пробела
         if (pos > 0) { 
-            w[k] = (char*)malloc((pos + 200) * sizeof(char)); 
-            strcpy(w[k], t, pos); 
-            w[k++][pos] = ' \0'; 
-        } 
+            W[k] = (char*)malloc((pos + 200) * sizeof(char)); 
+            strcpy(W[k], t, pos); 
+            W[k++][pos] = ' \0'; } 
         strcpy(t, t + 1 + pos); 
     } 
-    printf("Words in string:\n"); 
-    for (i = 0; i < k; ++i) { 
-        printf("%d. %s\n", i + 1, w[i]); 
-    } 
+    printf("Words in string:\n"); for (i = 0; i < k; ++i) { printf("%d. %s\n", i + 1, W[i]); } 
 }
