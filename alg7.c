67446f736a2b7c913513aab587897cf42e3929b7
sofyakapanova <105154076+sofyakapanova@users.noreply.github.com>
2024-03-25 01:12:29 +0300

    Update alg7.c
    сделала компактнее

--- alg7.c
+++ alg7.c
@@ -2,40 +2,28 @@
 #include <string.h> 
 #include <stdlib.h> 
 int main() { 
- char **name, **list; int* reit; int n, k, i, l, j, t; char tmp[198]; 
+ char **Name, **List; int* Reit; int n, k, i, l, j, t; char tmp[198]; 
  printf("Press n: "); scanf("%d", &n); 
- name = (char**)malloc(n * sizeof(char*)); 
- list = (char**)malloc(n * sizeof(char*)); 
- reit = (int*)malloc(n * sizeof(int)); 
+ Name = (char**)malloc(n * sizeof(char*)); List = (char**)malloc(n * sizeof(char*)); Reit = (int*)malloc(n * sizeof(int)); 
  printf("Press data\n\n"); 
  for (i = 0; i < n; i++) { 
-  printf("Press name: "); 
-  scanf("%s", tmp); 
-  name[i] = (char*)malloc((strlen(tmp) + 2)*sizeof(char)); 
-  strcpy(name[i], tmp);
+  printf("Press Name: "); scanf("%s", tmp); 
+  Name[i] = (char*)malloc((strlen(tmp) + 2)*sizeof(char)); 
+  strcpy(Name[i], tmp);
  } 
  k = 0; 
  for (i = 0; i < n; i++) { 
   l = -1; 
   j = 0; 
   while ((l == -1) && (j < k)) { 
-   t = strcmp(list[j], name[i]); 
-   if (t == 0) { 
-    l = j; 
-   } 
-   j++; 
-  } 
+   t = strcmp(List[j], Name[i]); 
+   if (t == 0) { l = j; } 
+   j++; } 
   if (l == -1) { 
-   list[k] = (char*)malloc((strlen(name[i]) + 2) * sizeof(char)); 
-   strcpy(list[k], name[i]); 
-   reit[k] = 1; k++;
-  } 
-  else { 
-   reit[l]++;
-  } 
+   List[k] = (char*)malloc((strlen(Name[i]) + 2) * sizeof(char)); 
+   strcpy(List[k], Name[i]); 
+   Reit[k] = 1; k++;
+  } else { Reit[l]++;} 
  } 
- printf("\nList without repeat and her reit: \n"); 
- for (i = 0; i < k; i++) { 
-  printf("%s  %d \n", list[i], reit[i]); 
- } 
+ printf("\nList without repeat and her Reit: \n"); for (i = 0; i < k; i++) { printf("%s  %d \n", List[i], Reit[i]); } 
 }
