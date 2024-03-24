c48d597a8477deab1fdc99a69193679d69aed5a0
sofyakapanova <105154076+sofyakapanova@users.noreply.github.com>
2024-03-25 01:09:12 +0300

    Update alg6.c
    добавила описание

--- alg6.c
+++ alg6.c
@@ -2,37 +2,26 @@
 #include <string.h> 
 #include <stdlib.h> 
 int main() { 
- char **name, **list; 
- int n, k, i, l, j, t; 
- char tmp[198]; 
+ char **Name, **List; int n, k, i, l, j, t; char tmp[198]; 
  printf("Press n: "); scanf("%d", &n); 
- name = (char**)malloc(n * sizeof(char*)); list = (char**)malloc(n * sizeof(char*)); 
+ Name = (char**)malloc(n * sizeof(char*)); List = (char**)malloc(n * sizeof(char*)); 
  printf("Press data\n\n"); 
  for (i = 0; i < n; i++) { 
-  printf("Press name: "); 
-  scanf("%s", tmp); 
-  name[i] = (char*)malloc((strlen(tmp) + 2)*sizeof(char)); 
-  strcpy(name[i], tmp);
+  printf("Press Name: "); scanf("%s", tmp); 
+  Name[i] = (char*)malloc((strlen(tmp) + 2)*sizeof(char)); 
+  strcpy(Name[i], tmp);
  } 
  k = 0; 
- for (i = 0; i < n; i++) { 
-  l = -1; 
-  j = 0; 
+ for (i = 0; i < n; i++) { // для каждого из Name
+  l = -1; j = 0; 
   while ((l == -1) && (j < k)) { 
-   t = strcmp(list[j], name[i]); 
-   if (t == 0) { 
-    l = j; 
-   } 
-   j++; 
-  } 
-  if (l == -1) { 
-   list[k] = (char*)malloc((strlen(name[i]) + 2) * sizeof(char)); 
-   strcpy(list[k], name[i]);
-   k++;
-  }
+   t = strcmp(List[j], Name[i]); // = 0 if совп.
+   if (t == 0) { l = j; } // if совп. то меняем флаг (обозначаем что нашли Name[i] в List)
+   j++; } // перебираем дальше
+  if (l == -1) { // if не нашли Name[i] в List
+   List[k] = (char*)malloc((strlen(Name[i]) + 2) * sizeof(char)); 
+   strcpy(List[k], Name[i]); // добавляем его в List
+   k++; } // увел. число эл-в в List
  } 
- printf("\nList without repeat and her data: \n"); 
- for (i = 0; i < k; i++) { 
-  printf("%s \n", list[i]); 
- } 
+ printf("\nList without repeats and her data: \n"); for (i = 0; i < k; i++) { printf("%s \n", List[i]); } 
 }
